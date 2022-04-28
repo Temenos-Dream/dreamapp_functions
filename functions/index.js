301d0c290cf5135aebb34036d9caf87652011d4b
Nes Croft <nes.croft@gmail.com>
2022-04-28 13:45:32 -0400

    various new functions

--- functions/index.js
+++ functions/index.js
@@ -2,13 +2,64 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+//test commands
+//firebase functions:shell
+//test()
+exports.test = functions.https.onRequest(async (request, response) => {
+  console.log("testing sort friends lists");
+});
+
+function date_sortFunction(a, b) {
+  var dateA = new Date(a.created).getTime();
+  var dateB = new Date(b.created).getTime();
+  return dateA > dateB ? -1 : 1;
+}
+
+async function sortFriendsLists() {
+  await admin
+    .firestore()
+    .collection("Users")
+    .get()
+    .then((snapshot) => {
+      users = snapshot.docs;
+      users.forEach(async (user) => {
+        user = user.data();
+        if (user.friends.length > 10) {
+          let sortedFriendsList = [];
+          let newFriendList = [];
+          console.log(user.profileCore.username + " " + user.id);
+          for (let i = 0; i < user.friends.length; i++) {
+            let friendId = user.friends[i];
+            let dreamSnapshot = await admin
+              .firestore()
+              .collection("PublicDreams")
+              .where("uid", "==", friendId)
+              .orderBy("created", "desc")
+              .limit(2)
+              .get();
+            dreams = dreamSnapshot.docs;
+            let latestDreamTime = "2020-01-01T00:00:00.000Z"; // old dream
+            if (dreams.length > 0) {
+              latestDreamTime = dreams[0].data().created;
+              console.log("found dream of " + friendId);
+            }
+            sortedFriendsList.push({
+              id: friendId,
+              created: latestDreamTime,
+            });
+          }
+          sortedFriendsList = sortedFriendsList.sort(date_sortFunction);
+          newFriendList = sortedFriendsList.map((item) => item.id);
+          console.log(JSON.stringify(newFriendList, null, 2));
+          await admin
+            .firestore()
+            .collection("Users")
+            .doc(user.id)
+            .update({ friends: newFriendList });
+        }
+      });
+    });
+}
 
 const autoId = () => {
   const CHARS =
@@ -31,16 +82,29 @@ const smartTruncate = (value, length, endString = "...") => {
   return value.trim();
 };
 
-exports.shareDream = functions.firestore
+exports.publicDreamCreate = functions.firestore
   .document("PublicDreams/{docId}")
   .onCreate(async (snap, context) => {
     const doc = snap.data();
     if (doc.privacy == "shared") {
       shareDreamCreateActivities(doc);
+    } else {
+      // createDream_NewActivity_ForFriends(doc);
     }
+    sortFriendsLists();
   });
 
-exports.shareDreamUpdate = functions.firestore
+async function createDream_NewActivity_ForFriends(doc) {
+  let userWhoPostedDream = await admin
+    .firestore()
+    .collection("Users")
+    .doc(doc.uid)
+    .get();
+  if (userWhoPostedDream && userWhoPostedDream.friends) {
+  }
+}
+
+exports.publicDreamUpdate = functions.firestore
   .document("PublicDreams/{docId}")
   .onUpdate(async (change, context) => {
     // ...the new value after this update
@@ -51,6 +115,16 @@ exports.shareDreamUpdate = functions.firestore
     if (prev.privacy != "shared" && after.privacy == "shared") {
       shareDreamCreateActivities(after);
     }
+
+    await admin
+      .firestore()
+      .collection("Users")
+      .get()
+      .then((snapshot) => {
+        // console.log(snapshot.docs.length);
+        users = snapshot.docs;
+        users.forEach(() => {});
+      });
   });
 
 function shareDreamCreateActivities(doc) {
@@ -79,22 +153,36 @@ exports.commentCreate = functions.firestore
   .document("Comments/{docId}")
   .onCreate(async (snap, context) => {
     let doc = snap.data();
-    let query = await admin
+    await admin
       .firestore()
-      .collection("PublicDreams")
-      .doc(doc.dreamId)
-      .update({ commentCount: admin.firestore.FieldValue.increment(1) });
+      .collection("Comments")
+      .where("dreamId", "==", doc.dreamId)
+      .get()
+      .then(async (snapshot) => {
+        await admin
+          .firestore()
+          .collection("PublicDreams")
+          .doc(doc.dreamId)
+          .update({ commentCount: snapshot.docs.length });
+      });
   });
 
 exports.commentDelete = functions.firestore
   .document("Comments/{docId}")
   .onDelete(async (snap, context) => {
     let doc = snap.data();
-    let query = await admin
+    await admin
       .firestore()
-      .collection("PublicDreams")
-      .doc(doc.dreamId)
-      .update({ commentCount: admin.firestore.FieldValue.increment(-1) });
+      .collection("Comments")
+      .where("dreamId", "==", doc.dreamId)
+      .get()
+      .then(async (snapshot) => {
+        await admin
+          .firestore()
+          .collection("PublicDreams")
+          .doc(doc.dreamId)
+          .update({ commentCount: snapshot.docs.length });
+      });
   });
 
 exports.dreamInfoCreate = functions.firestore //dreaminfo stores data about the dream we don't want to pull from the cloud all the time. because it would be too much data and slow down the app.
@@ -105,7 +193,7 @@ exports.dreamInfoCreate = functions.firestore //dreaminfo stores data about the 
       .firestore()
       .collection("PublicDreams")
       .doc(doc.id)
-      .update({ viewCount: admin.firestore.FieldValue.increment(1) });
+      .update({ viewCount: 1 });
   });
 
 exports.dreamInfoUpdate = functions.firestore //dreaminfo stores data about the dream we don't want to pull from the cloud all the time. because it would be too much data and slow down the app.
@@ -132,41 +220,42 @@ exports.commentAction = functions.firestore
     const doc = change.after.data() || {};
     // ...the previous value before this update
     const previousValue = change.before.data() || {};
-    let activity = {
-      id: autoId(),
-      type: "upvote",
-      message: "",
-      created: new Date(),
-      for_id: doc.authorId,
-      from_id: "",
-      from_name: "Upvote",
-      from_pic: "/other/upvote_one.png",
-      dreamId: doc.dreamId,
-      comment: doc,
-    };
-    if (doc.score == 2) {
-      activity.message = "1st upvote! ⬆️";
-    } else if (doc.score == 11) {
-      activity.message = "10th upvote! ⬆️";
-    } else if (doc.score == 26) {
-      activity.message = "25th upvote! ⬆️";
-    } else if (doc.score == 101) {
-      activity.message = "100th upvote! 🎉";
-    } else if (doc.score == 501) {
-      activity.message = "500th upvote! 🤯";
-    } else if (doc.score == 1001) {
-      activity.message = "1000th upvote! 🤩";
+    if (doc.score > previousValue.score) {
+      let activity = {
+        id: autoId(),
+        type: "upvote",
+        message: "",
+        created: new Date(),
+        for_id: doc.authorId,
+        from_id: "",
+        from_name: "Upvote",
+        from_pic: "/other/upvote_one.png",
+        dreamId: doc.dreamId,
+        comment: doc,
+      };
+      if (doc.score == 2) {
+        activity.message = "1st upvote! ⬆️";
+      } else if (doc.score == 11) {
+        activity.message = "10th upvote! ⬆️";
+      } else if (doc.score == 26) {
+        activity.message = "25th upvote! ⬆️";
+      } else if (doc.score == 101) {
+        activity.message = "100th upvote! 🎉";
+      } else if (doc.score == 501) {
+        activity.message = "500th upvote! 🤯";
+      } else if (doc.score == 1001) {
+        activity.message = "1000th upvote! 🤩";
+      }
+      if (activity.message !== "") {
+        activity.message +=
+          ' View your comment "' + smartTruncate(doc.text, 100) + '"';
+        let query = await admin
+          .firestore()
+          .collection("Activity")
+          .doc(activity.id)
+          .set(activity);
+      }
     }
-    if (activity.message !== "") {
-      activity.message +=
-        ' View your comment "' + smartTruncate(doc.text, 100) + '"';
-      let query = await admin
-        .firestore()
-        .collection("Activity")
-        .doc(activity.id)
-        .set(activity);
-    }
-    console.log(doc);
   });
 
 exports.newActivity = functions.firestore
@@ -182,12 +271,15 @@ exports.newActivity = functions.firestore
       .get();
 
     if (query) {
-      const user = query.data();
+      const user = query.data(); // <--- user the activity is for
       admin
         .firestore()
         .collection("UserInfo")
         .doc(user.id)
-        .update({ activityCount: admin.firestore.FieldValue.increment(1) });
+        .set(
+          { activityCount: admin.firestore.FieldValue.increment(1) },
+          { merge: true }
+        );
       if (user.notificationsOn && user.tokens) {
         const tokens = user.tokens;
         const payload = {
